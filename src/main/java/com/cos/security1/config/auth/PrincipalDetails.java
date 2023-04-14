e2c26354210cfa02f6f1bf3783c4672d519f5e94
Griotold <gotjd9773@naver.com>
2023-04-14 11:38:25 +0900

    oauth2 - Authentication이 담을 수 있는 2가지 타입

--- src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
+++ src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
@@ -1,14 +1,18 @@
 package com.cos.security1.config.auth;
 
 import com.cos.security1.model.User;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 // Security Session => Authentication => UserDetails (PrincipalDetails)
-public class PrincipalDetails implements UserDetails {
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user; // composition
 
@@ -16,11 +20,21 @@ public class PrincipalDetails implements UserDetails {
         this.user = user;
     }
 
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+    @Override
+    public String getName() {
+        return null;
+    }
+
     // 권한을 리턴하는 함수
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Collection<GrantedAuthority> collect = new ArrayList<>();
         // 자바 8 방식을 사용해도 되는데 아직 공부가 부족하니 이대로 가자.
+        // return Collections.singletonList(() -> user.getRole());
         collect.add(new GrantedAuthority() {
             @Override
             public String getAuthority() {
@@ -60,4 +74,6 @@ public class PrincipalDetails implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+
 }
--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,11 +1,16 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.config.auth.PrincipalDetails;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
@@ -21,6 +26,32 @@ public class IndexController {
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    // 유저 정보를 가져오는 두가지 방법!
+    @GetMapping("/test/login")
+    public @ResponseBody String testLogin(Authentication authentication,
+                                          @AuthenticationPrincipal PrincipalDetails userDetails) {
+        System.out.println("/test/login ==========");
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        System.out.println("principalDetails.getUser() = " + principalDetails.getUser());
+
+        System.out.println("userDetails.getUsername() = " + userDetails.getUser());
+        System.out.println("두 방법 모두 유저 정보를 가져오는 방법이다.");
+        return "세션 정보 확인하기";
+    }
+
+    // oauth는 아래 방식으로 해야 유저 정보를 가져올 수 있음
+    // 역시 두 가지 방법
+    @GetMapping("/test/oauth/login")
+    public @ResponseBody String testOauthLogin(Authentication authentication,
+                                               @AuthenticationPrincipal OAuth2User oauth) {
+        System.out.println("/test/oauth/login ==========");
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        System.out.println("oAuth2User.getAttributes() = " + oAuth2User.getAttributes());
+        System.out.println("oauth.getAttributes() = " + oauth.getAttributes());
+        
+
+        return "OAuth 세션 정보 확인하기";
+    }
     @GetMapping({"", "/"})
     public String index() {
         return "index";
